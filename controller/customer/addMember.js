4c14ca3fa0ad7710c9c223c4d0a0d999bc29d50e
shubhampatel464 <94957047+shubhampatel464@users.noreply.github.com>
2024-05-12 13:38:55 +0000

    19

--- controller/customer/addMember.js
+++ controller/customer/addMember.js
@@ -3,42 +3,53 @@ const mongoose = require("mongoose");
 const group = require("../../models/customer/group");
 
 
-const addMember = async (req,res) => {
+const addMember = async (req, res) => {
 
     try {
 
         const data = req.body;
-        const ifExist = await group.findOne({id:data.group_id});
+        const ifExist = await group.findOne({ id: data.group_id });
         // console.log("******");
 
-        if(!ifExist){
+        if (!ifExist) {
 
-            const data = new group({
-                id:Number(data.group_id)
-            })
+            try {
 
-            const saved_data = await data.save();
+                const GroupData = new group({
+                    id: Number(data.group_id)
+                })
+
+                const saved_data = await GroupData.save();
+
+            } catch (error) {
+                console.log("This is error from /controller/customers/addMember.js ((group creation part))");
+                console.log(error);
+                res.status(500).send({ "error": "Internal Server Error" });
+            }
+
+
 
         }
 
         const member = {
-            agent_id : data.agent_id,
-            name : data.name,
-            mobile : Number(data.mobile),
-            email : data.email,
-            dob : data.dob
+            agent_id: data.agent_id,
+            name: data.name,
+            mobile: Number(data.mobile),
+            email: data.email,
+            dob: data.dob
         }
 
-        const update = await group.findOneAndUpdate({id:data.group_id},{$push : { members : member }},{new:true});
+        const update = await group.findOneAndUpdate({ id: data.group_id }, { $push: { members: member } }, { new: true });
 
         const id = update.members[update.members.length - 1]._id;
-        
-        res.status(200).send({id:id});
+
+        res.status(200).send({ id: id });
 
 
     } catch (error) {
         console.log("This is error from /controller/customers/addMember.js");
         console.log(error);
+        res.status(500).send({ "error": "Internal Server Error" });
     }
 
 }
